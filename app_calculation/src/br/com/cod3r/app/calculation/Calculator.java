cccb0388f3b45af23ccd3e933dab68aed286aa8c
juanalenca <juanbarros.dev@gmail.com>
2024-08-27 21:38:36 -0300

    Entendo sobre o "transitive" e o "exports to"

--- app_calculation/src/br/com/cod3r/app/calculation/Calculator.java
+++ app_calculation/src/br/com/cod3r/app/calculation/Calculator.java
@@ -1,13 +1,15 @@
 package br.com.cod3r.app.calculation;
 
 import br.com.cod3r.app.calculation.internal.ArithmericOperations;
+import br.com.cod3r.app.logging.Logger;
 
 public class Calculator {
 	
 	private ArithmericOperations aritOperations = new ArithmericOperations();
 
 	public double sum(double... nums) {
+		Logger.info("Somando");
 		return aritOperations.sum(nums);
 	}
-
+	
 }
--- app_calculation/src/module-info.java
+++ app_calculation/src/module-info.java
@@ -1,8 +1,20 @@
-//modulos agrupam conjuntos de pacotes e pacotes agrupam conjuuntos de classes
+//modulos agrupam conjuntos de pacotes e pacotes agrupam conjuntos de classes
 module app.calculation {
+	
+	//requerendo o pacote logging
+	//o transitive é para marcar que o pacote que foi requerido pode ser acessado para outros modulos terceirros
+	requires transitive app_logging;
 	
 	//exportando o arquivo para acessar a partir de outros módulos
 	//e não pode haver depedÊncia cíclica
 	exports br.com.cod3r.app.calculation;
+	
+	/*
+	Você não está dependendo do modulo financial, vocÊ está apenas
+	abrindo a possibilidade do modulo financial acessar as classes internal
+	dentro do modulo calculation. Se quiser colocar mais modulos que terão
+	acesso a esse pacote basta separar por virgula
+	*/
+	exports br.com.cod3r.app.calculation.internal to app.financial;
 	
 }
